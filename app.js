40c2bdb9b17894ad84991466e8792cbacc9cdafd
teknofage <teknofage@gmail.com>
2021-03-30 16:59:15 -0700

    adding middleware

--- app.js
+++ app.js
@@ -12,8 +12,18 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
-});
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
+})
 
 // Start Server
 
