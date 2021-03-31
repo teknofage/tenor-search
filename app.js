5f007820a7bab08822dfdd082df6aa3aef85f54d
teknofage <teknofage@gmail.com>
2021-03-30 17:11:22 -0700

    added search form

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
+// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "{ term: hey" }
+
+  res.render('home')
 })
 
 app.get('/greetings/:name', (req, res) => {
