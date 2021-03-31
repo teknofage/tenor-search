1e75f0795d9cc8c43f73a3d6bc38a672771755b7
teknofage <teknofage@gmail.com>
2021-03-30 18:10:21 -0700

    added working gif search, hid API key in .env file

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // Require Libraries
 const express = require('express');
 
+const dotenv = require('dotenv');
+dotenv.config();
+require('dotenv').config();
+const TENOR_API_KEY = process.env.TENOR_API_KEY;
+
+const Tenor = require("tenorjs").client({
+  "Key": TENOR_API_KEY, // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // App Setup
 const app = express();
 
@@ -11,11 +22,20 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
-// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  console.log(req.query) // => "{ term: hey" }
-
-  res.render('home')
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          // store the gifs we get back from the search
+          const gifs = response;
+          // pass the gifs as an object into the home page
+          res.render('home', { gifs })
+      }).catch(console.error);
 })
 
 app.get('/greetings/:name', (req, res) => {
